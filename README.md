09e7d4c59f24996ca1b63b0eb7a7982cd0b155a9
Luiz Guilherme <aoliveira.luizguilherme@gmail.com>
2026-01-27 21:44:37 -0300

    feat: implementacao dos campos da entidade Task e adicao de dependencias no README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # task-manager
 Um sistema para gerenciar tarefas pessoais, permitindo a criação, atualização, exclusão e listagem de tarefas, com suporte a prioridades e categorias.
+
+# Dependencias do Projeto e sua Utilidade
+
+- Spring Web: Para criar a API REST
+- Spring Data JPA: Para o banco de dados
+- H2 Database: Banco de dados em memória do projeto
+- Validation: Para validar os campos
+- Lombok: Para reduzir código repetitivo como Getters e Setters
---
+++ src/main/java/com/luiz/guilherme/taskmanager/task_manager/model/Task.java
@@ -0,0 +1,27 @@
+package com.luiz.guilherme.taskmanager.task_manager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private boolean completed = false;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = true)
+    private LocalDateTime finishedAt = null;
+}
