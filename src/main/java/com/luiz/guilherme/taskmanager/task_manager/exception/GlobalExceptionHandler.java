e5ba09ddb4489de599c961ff732dcb6e0d2d051e
Luiz Guilherme <aoliveira.luizguilherme@gmail.com>
2026-01-29 15:17:19 -0300

    feat: implementacao do mecanismo global de tratamento de excecoes

---
+++ src/main/java/com/luiz/guilherme/taskmanager/task_manager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.luiz.guilherme.taskmanager.task_manager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Erro na Requisição");
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
